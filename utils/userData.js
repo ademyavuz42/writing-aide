2bd86ce68fffe9903b407e226b4b8ee5c1455b9c
ademyavuz42 <ademyavuz@gmail.com>
2025-02-27 23:52:52 +0700

    Login Page added

---
+++ utils/userData.js
@@ -0,0 +1,30 @@
+const userData = {
+    superAdmin: {
+        email: 'ademyavuz@gmail.com',
+        password: 'writingadem'
+    },
+    admin: {
+        email: 'yavuzadem2000@yahoo.com',
+        password: 'writingadem'
+    },
+    teacher: {
+        email: 'yavuzadem2000@yahoo.com',
+        password: 'writingadem'
+    },
+    supervisor: {
+        email: 'yavuzadem2000@yahoo.com',
+        password: 'writingadem'
+    },
+    student: {
+        email: 'x84r9q5th7@expressletter.net',
+        password: 'Writing2.'
+    },
+    invalidUser: {
+        missingAtEmail: 'invalidEmail.com',
+        missingDomainEmail: 'invalid@',
+        invalidEmail: 'unknownuser@gmail.com',
+        shortPasswort: '123',
+        incorrectPassword: 'WrongPass'
+    }
+};
+module.exports = userData;
