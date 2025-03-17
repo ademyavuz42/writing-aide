9fe5a7897b8c2e08db3805d574ce26d3d8079433
ademyavuz42 <ademyavuz@gmail.com>
2025-03-17 12:54:06 +0700

    Student Dashboard

---
+++ pages/StudentDashboard.js
@@ -0,0 +1,69 @@
+const LoginPage = require('./LoginPage');
+
+class StudentDashboard extends LoginPage{
+    constructor(page) {
+        super(page)
+        this.page = page;
+        this.logo = page.locator('header img');
+        this.writingLink = page.getByRole('link', { name: 'Writings' });
+        this.ordersLink = page.getByRole('link', { name: 'Orders' });
+        this.creditLink = page.getByRole('link', { name: 'Credits' });
+        this.role = page.getByRole('button', { name: 'PERSONAL' });
+        this.creditAmount = page.locator('header .mantine-focus-auto.m_b6d8b162.mantine-Text-root').nth(4);
+        this.profile = page.getByRole('button', { name: 'Settings' });
+        this.setting = page.getByRole('menuitem', { name: 'Settings' }); 
+        this.logout = page.getByRole('menuitem', { name: 'Log Out' }); 
+        this.welcomeMessage = page.getByRole('heading', { name: 'Hi, ' });
+        this.roleDefault = page.getByRole('button', { name: 'PERSONAL' })
+        this.rolePersonal = page.getByRole('menuitem', { name: 'PERSONAL' });
+        this.roleStudent = page.getByRole('menuitem', { name: 'STUDENT' });
+        this.getStarted = page.getByRole('link', { name: 'Get Started' });
+        this.recentWriting = page.getByRole('heading', { name: 'Recent Writings' });
+        this.rating = page.getByRole('button', { name: 'Rating & Review' });
+        this.examTypeLocator = page.locator('div#writingTest > span.mantine-Badge-label');
+
+    }
+
+    async navigate() {
+        await this.page.goto('/login');
+    }
+
+    async getWelcomeText() {
+        const text = await this.welcomeMessage.innerText();
+        return text;
+    }
+
+    async getCreditAmount() {
+            const amount = await this.creditAmount.innerText();
+            console.log(amount);
+            return amount;
+    }
+
+    async assertCreditIsNumber() {
+        const creditText = await this.creditAmount.innerText();
+        const creditNumber = parseInt(creditText.match(/\d+/)[0], 10); // Extract number
+        return creditNumber;
+    }
+    
+    async isElementVisible(element){
+        return await element.isVisible()
+    }
+
+    async getElementByRole(role, name){
+        return await this.page.getByRole(role, {name: name})
+    }
+
+    async getTestTitlesText(){
+        const counts = await this.examTypeLocator.count();
+        console.log('Number of tests found: ', counts);
+        const titles = [];
+        for (let i = 0; i < counts; i++) {
+            const title = await this.examTypeLocator.nth(i).innerText();
+            titles.push(title);
+        }
+        return titles;
+      }
+    
+   
+}
+module.exports = StudentDashboard;
---
+++ tests/studentDashBoard.spec.js
@@ -0,0 +1,131 @@
+const { test, expect, chromium } = require('@playwright/test');
+const StudentDashboard = require('../pages/StudentDashboard');
+const userData = require('../utils/userData');
+const exp = require('constants');
+const { execPath } = require('process');
+
+test.describe('Student Dashboard Page Tests', () => {
+    let browser, context, page, student;
+
+    test.beforeAll(async () => {
+        browser = await chromium.launch({ headless: false });
+    });
+
+    test.beforeEach(async () => {
+        context = await browser.newContext();
+        page = await context.newPage();
+        student = new StudentDashboard(page);
+        await student.navigate();
+        await student.login(userData.student.email, userData.student.password);
+    });
+
+    test.afterEach(async () => {
+        await page.close();
+    });
+
+    test.afterAll(async () => {
+        await browser.close();
+    });
+
+    test('Verify title and URL of Dashboard page', async () => {
+        await expect(page).toHaveURL('/dashboard');
+        await expect(page).toHaveTitle('Dashboard - Writing-Aide');
+    });
+
+    test('Verify the welcome message', async() => {
+        const welcomeMessage = await student.getWelcomeText();
+        console.log(welcomeMessage);
+        expect(welcomeMessage).toContain('Hi,');
+    })
+
+    test('Verify the credit amount displayed', async() => {
+        const creditAmount = await student.getCreditAmount();
+        expect(creditAmount).not.toBeNull();
+    })
+
+    test('Verify credit amount is a number', async () => {
+       const result = await student.assertCreditIsNumber();
+       expect(typeof result).toBe('number');
+      });
+
+      test('Verify logo is visible and clickablr', async ()=>{
+        await expect(student.logo).toBeVisible();
+        await student.logo.click();
+        await expect(page).toHaveURL('/');
+      });
+
+      test('Verify navigation links in the header', async()=>{
+        const navLinks = [
+           
+            {link: student.writingLink, url: '/writing'},
+            {link: student.ordersLink, url: '/order'},
+            {link: student.creditLink, url: '/credit'},
+        ];
+       
+        for(const {link, url} of navLinks){
+            await link.click();
+            await expect(page).toHaveURL(url);
+        }
+      })
+
+      test('Verify visibility of setting and logout button', async()=>{
+        await expect(student.profile).toBeVisible();
+        await student.profile.click();
+        await expect(student.setting).toBeVisible();
+        await expect(student.logout).toBeVisible();
+      })
+
+      test('Verify setting button functionality', async()=>{
+        await student.profile.click();
+        await student.setting.click();
+        await expect(page).toHaveURL('/profile')
+        await expect(page.getByText('Change Password')).toBeVisible();
+      })
+
+      test('Verify logout button functionality', async()=>{
+        await student.profile.click();
+        await student.logout.click();
+        await expect(page).toHaveURL('/')
+      })
+
+      test('Verify the roles are visible', async()=>{
+        await expect(student.roleDefault).toBeVisible();
+        await student.roleDefault.click();
+        await expect(student.rolePersonal).toBeVisible();
+        await expect(student.roleStudent).toBeVisible();
+      });
+
+    test('Verify the functionality of Get Started button', async()=>{
+        await expect(student.getStarted).toBeVisible();
+        await student.getStarted.click();
+        await expect(page).toHaveURL('/writing/start')
+    })
+
+    test('Verify Role Student button functionality', async()=>{
+        await expect(student.roleDefault).toBeVisible();
+        await student.roleDefault.click();
+        await expect(student.roleStudent).toBeVisible();
+        await student.roleStudent.click();
+        await expect(page).toHaveURL('/my-assignment');
+    })
+    
+    test('Verify the visibility of the recent writings', async()=>{
+        await expect(student.recentWriting).toBeVisible();
+    })
+
+    test('Verify the functionality of Rating button', async()=>{
+        await expect(student.rating).toBeVisible();
+    })
+
+    test('Verify the visibility of taken test titles', async()=>{
+        await page.waitForSelector('div.mantine-Badge-root > span.mantine-Badge-label', { timeout: 5000 });
+        await page.screenshot({ path: 'debug.png', fullPage: true });
+        await expect(page).toHaveScreenshot('debug.png', { fullPage: true });
+        const titles = await student.getTestTitlesText();
+        console.log('Test titles: ', titles);
+        expect(titles.length).toBeGreaterThan(0);
+    })
+
+  
+});
+
