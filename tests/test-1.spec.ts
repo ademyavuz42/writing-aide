6542050c09be10cc87c088f0418ea4186f348e2f
ademyavuz42 <ademyavuz@gmail.com>
2025-04-25 21:18:12 +0700

    writingpage update

--- tests/test-1.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  // Recording...
-  await expect(page.locator('.m_e615b15f > div:nth-child(5)')).toBeVisible();
-});
